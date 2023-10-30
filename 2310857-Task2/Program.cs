57df520f710e07ac1ea4244aa6184e596e4375ea
Tyler Mcmullen <59979897+CoronaCarrot@users.noreply.github.com>
2023-10-30 12:01:52 +0000

    Add project files.

---
+++ 2310857-Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace _2310857_Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) // Main Loop
+            {
+                // Take an input of only K, F or C. will not allow any other input
+                Temperature temp = new Temperature();
+                Console.Write("What measurement? (K, F or C):\n>>> ");
+                char measurement = 'u';
+                while (measurement == 'u')
+                {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    switch (Char.ToLower(key.KeyChar))
+                    {
+                        case 'k':
+                            measurement = 'K';
+                            break;
+
+                        case 'f':
+                            measurement = 'F';
+                            break;
+
+                        case 'c':
+                            measurement = 'C';
+                            break;
+                    }
+                }
+                Console.Write($"{measurement}\n");
+
+                // take temperature input
+                double? temperature = null;
+                while(temperature == null)
+                {
+                    Console.Write("What temperature?\n>>> ");
+                    string input = Console.ReadLine();
+                    try
+                    {
+                        temperature = Convert.ToDouble(input); // attept to convert temperature
+                        break;
+                    }
+                    catch
+                    {
+                        // if not valid number output error
+                        Console.WriteLine("Invalid Input!\n");
+                    }
+                }
+
+                temp.Set(Convert.ToDouble(temperature), measurement); // set and auto convert temperature to to the users chosen input
+
+                Console.WriteLine($"\nCelcius: {temp.Celcius}\nFahrenheit: {temp.Fahrenheit}\nKelvin: {temp.Kelvin}\n\n"); // print out temperatures
+
+            }
+        }
+    }
+}
---
+++ 2310857-Task2/Temperature.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2310857_Task2
+{
+    internal class Temperature
+    {
+        // define internal variables + their public readonly counterparts
+        // readonly to make sure only Temperature.Set() can be used
+        private double celcius;
+        public double Celcius
+        {
+            get { return this.celcius; }
+        }
+        private double kelvin;
+        public double Kelvin
+        {
+            get { return this.kelvin; }
+        }
+        private double fahrenheit;
+        public double Fahrenheit
+        {
+            get { return this.fahrenheit; }
+        }
+
+        // Class initialisation function
+        public Temperature(double temperature = 0, char type = 'C')
+        {
+            Set(temperature, type);
+        }
+
+        public void Set(double temperature = 0, char type = 'C')
+        {
+            switch (Char.ToLower(type))
+            {
+                case 'k':
+                    KelvinTo(temperature);
+                    break;
+
+                case 'f':
+                    FahrenheitTo(temperature);
+                    break; 
+
+                case 'c':
+                    CelciusTo(temperature);
+                    break;
+            }
+        }
+
+        // functions to auto convert Temperature.Set() inputs to all measurements
+        private void KelvinTo(double temperature)
+        {
+            this.kelvin = temperature;
+            this.celcius = temperature - 273.15;
+            this.fahrenheit = (this.celcius * 9 / 5) + 32;
+        }
+
+        private void FahrenheitTo(double temperature)
+        {
+            this.fahrenheit = temperature;
+            this.celcius = (this.fahrenheit - 32) * 5/9;
+            this.kelvin = this.celcius + 273.15;
+
+        }
+        private void CelciusTo(double temperature)
+        {
+            this.celcius = temperature;
+            this.kelvin = this.celcius + 273.15;
+            this.fahrenheit = (this.celcius * 9 / 5) + 32;
+        }
+
+    }
+}
